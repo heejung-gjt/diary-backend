e340f01c11d7c1d964f2b61e6a907177f329b8f0
heejung-gjt <hee_9803@naver.com>
2021-11-27 15:47:21 +0900

    refactor: post user logic
    - 회원가입, 로그인 로직 수정
    - 회원가입, 로그인 기능 유닛 테스트 구현

--- core/validator.py
+++ core/validator.py
@@ -1,16 +1,34 @@
+import bcrypt
+
 from django.core.exceptions import ValidationError
 
 from user.models import User
 
+
 def validate_id(id):
     if User.objects.filter(userid = id).exists():
-        raise ValidationError(('아이디가 존재합니다 !'), code = 'invalid')
+        raise ValidationError(('아이디가 존재합니다'), code='invalid')
+
     if len(id) < 4:
-        raise ValidationError(('아이디 확인해주세요 !'), code = 'invalid')
+        raise ValidationError(('아이디 형식이 틀립니다'), code='invalid')
     return id
 
 
 def validate_pwd(pwd):
     if len(pwd) < 5:
-        raise ValidationError(('비밀번호 확인해주세요 !'), code = 'invalid')
+        raise ValidationError(('비밀번호 형식이 틀립니다'), code='invalid')
     return pwd
+
+
+def validate_user_id(id):
+    if not User.objects.filter(userid=id).exists():
+        raise ValidationError(("아이디 또는 비밀번호를 다시 입력해주세요"), code="invalid")
+    
+    return User.objects.get(userid=id)
+
+
+def validate_user_pwd(user, pwd):
+    if not bcrypt.checkpw(pwd.encode('utf-8'), user.password.encode('utf-8')):
+        raise ValidationError(("아이디 또는 비밀번호를 다시 입력해주세요"), code="invalid")
+    
+    return True
