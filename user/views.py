363e5abfb3b0cb593544a941ed6ed3e709c9f1df
heejung-gjt <hee_9803@naver.com>
2021-11-27 14:17:22 +0900

    feat: add core app
    - core models에 timestamp 생성
    - view에서 timestamp 적용 변경
    - validator 파일 core앱으로 이동
    - code convention 추가

--- user/views.py
+++ user/views.py
@@ -1,13 +1,34 @@
 import json
 import jwt
 import bcrypt
-from django.core.exceptions import ValidationError
-from django.http.response import JsonResponse
-from django.views.generic import View
-from config.settings import SECRET_KEY
-from .validator import validate_id, validate_pwd
 
-from .models import User
+from django.views.generic import View
+from django.http.response import JsonResponse
+from django.core.exceptions import ValidationError
+
+from user.models import User
+from core.validator import validate_id, validate_pwd
+from config.settings import SECRET_KEY
+
+
+class SignUpView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            userid = validate_id(data['username'])
+            password = validate_pwd(data['password'])
+            hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            User.objects.create(
+                userid = userid,
+                password = hashed_pwd
+            )
+            return JsonResponse({'message':'Success'}, status=200)
+        
+        except KeyError:
+            return JsonResponse({'message': 'Invalid Keys'}, status=400)
+
+        except ValidationError as e:
+            return JsonResponse({'error': e.message}, status=400)
 
 
 class LoginView(View):
@@ -26,22 +47,4 @@ class LoginView(View):
             return JsonResponse({'message': 'Invalid Keys'}, status=400)
 
 
-class SignUpView(View):
 
-    def post(self, request):
-        data = json.loads(request.body)
-        try:
-            userid = validate_id(data['username'])
-            password = validate_pwd(data['password'])
-            hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-            User.objects.create(
-                userid = userid,
-                password = hashed_pwd
-            )
-            return JsonResponse({'message':'Success'}, status=200)
-        
-        except KeyError:
-            return JsonResponse({'message': 'Invalid Keys'}, status=400)
-
-        except ValidationError as e:
-            return JsonResponse({'error': e.message}, status=400)
